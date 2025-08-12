9a87db005d04892ca607937c64760806f380f195
Roronoa <roranoazoro54@gmail.com>
2025-08-12 16:24:53 +0100

    Main menu added

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int menu_option;
+    cout << "Main Menu" << endl;
+    cout << "1. Add Exepense" << endl;
+    cout << "2. Remove Exepense" << endl;
+    cout << "3. Exit" << endl;
+    cout << "\nPlease enter a no (1-3): ";
+    cin >> menu_option;
+}
